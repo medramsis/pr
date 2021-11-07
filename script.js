db8a8e3dae9f3cce923b32d26f0fcdb3e61a9c53
med <medram90@gmail.com>
2021-11-07 12:38:47 +0100

    master

--- script.js
+++ script.js
@@ -56,3 +56,71 @@ alert('Bonjour '+lireCookie('CookieNom'));
  }
 
 
+ function formCheck() {
+  var arr = [];
+  var input = document.getElementById('nom').value;
+  var input2 = document.getElementById('prenom').value;
+  var input3 = document.getElementById('age').value;
+  var input4 = document.getElementById('email').value;
+  var input5 = document.getElementById('tel').value;
+  var val1 = input.length;
+  var val2 = input2.length;
+  var val3 = input3.length;
+  var val4 = input4.length;
+  var val5 = input5.length;
+
+ if (val1 > 0 ) {
+   document.getElementById('nom').style.border = "1px solid #e6e6e6";
+   input = true;
+   arr.push(input);
+ } 
+  
+  else {
+   document.getElementById('nom').style.border = "1px solid #ff4500";
+ }
+  
+  if (val2 > 0 ) {
+   input2 = true;
+   arr.push(input2);
+   document.getElementById('prenom').style.border = "1px solid #e6e6e6";
+ } 
+  
+  else {
+   document.getElementById('prenom').style.border = "1px solid #ff4500";
+ }
+ if (val3 > 0 ) {
+  input3 = true;
+  arr.push(input3);
+  document.getElementById('age').style.border = "1px solid #e6e6e6";
+} 
+ 
+ else {
+  document.getElementById('age').style.border = "1px solid #ff4500";
+}
+if (val4 > 0 ) {
+  input4 = true;
+  arr.push(input4);
+  document.getElementById('email').style.border = "1px solid #e6e6e6";
+} 
+ 
+ else {
+  document.getElementById('email').style.border = "1px solid #ff4500";
+}
+if (val5 > 0 ) {
+  input5 = true;
+  arr.push(input5);
+  document.getElementById('tel').style.border = "1px solid #e6e6e6";
+} 
+ 
+ else {
+  document.getElementById('tel').style.border = "1px solid #ff4500";
+}
+  
+  if(arr.length == 5) {
+    console.log("form submitted");
+    document.getElementById('demo3').innerHTML = "<p>Form Submitted</p>";
+  } else {
+    document.getElementById('demo3').innerHTML = "<p>Missing information!</p>";
+  }
+}
+
