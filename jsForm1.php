4e1eff8daaf896914167514a290ac21a843c20c2
med <medram90@gmail.com>
2021-11-24 11:10:43 +0100

    master

--- jsForm1.php
+++ jsForm1.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/stylejs.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
     <title>Document</title>
@@ -57,7 +56,7 @@ var myCookies= [];
     }
 }
     </script>
-<body>
+<body style="backgrounf">
 
         <form action="jsForm2.php"  id="survey-form">
             <div class="form-group">
